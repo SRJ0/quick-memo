3aa6f8c03edb67b40cbf089fad5ba67b15b708f6
josh <dev.sr.jo2@gmail.com>
2025-03-20 00:18:30 +0900

    feat: netlify function 구현, neon 연결

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,64 @@
-import Image from "next/image";
+"use client"
+import { useState } from "react";
+
+interface Result {
+    id: number;
+    subject: string;
+    memo: string;
+    grade: number;
+}
+
+const getBGColor = (grade: number) => {
+  if (grade === 1) return "red";
+  if (grade === 2) return "orange";
+  if (grade === 3) return "yellow";
+  if (grade === 4) return "green";
+  if (grade === 5) return "blue";
+}
 
 export default function Home() {
+  const [inputValue, setInputValue] = useState("");
+  const [results, displayResults] = useState<Result[]>([]);
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+  const handleButtonClick = () => {
+    fetchAndDisplay(inputValue);
+  };
+  const fetchAndDisplay = async (value: string) => {
+    const response = await fetch(`/.netlify/functions/fetchResults?query=${value}`);
+    const results = await response.json();
+    displayResults(results);
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-start justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <input
+              type="text"
+              className="border border-gray-300 rounded p-2"
+              placeholder="Enter text here"
+              value={inputValue}
+              onChange={handleInputChange}
+          />
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+              onClick={handleButtonClick}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+            Search
           </a>
         </div>
+        <div className="mt-4">
+          {results.map((result) => (
+              <div key={result.id}>
+                <p style={{backgroundColor: getBGColor(result.grade)}}>{result.subject}</p>
+                <p>{result.memo}</p>
+              </div>
+          ))}
+        </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
---
+++ netlify/functions/fetchResults.tsx
@@ -0,0 +1,35 @@
+import pg from "pg";
+const { Client } = pg;
+
+// interface QueryEvent {
+//     queryStringParameters: {
+//         query: string;
+//     };
+// }
+
+export const handler = async (event) => {
+    const client = new Client({
+        connectionString: process.env.DB_CONNECT_STRING,
+    })
+    try {
+        await client.connect();
+        const query = event.queryStringParameters.query;
+
+        const res = await client.query(
+            "SELECT * FROM INFO WHERE subject LIKE $1", [`${query}%`]
+        );
+
+        await client.end();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(res.rows)
+        };
+    } catch (error) {
+        console.log(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify(error)
+        };
+    }
+};
